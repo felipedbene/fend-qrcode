e96b5415bfd120d7da217c9aa806df973398d1c5
Ubuntu <ubuntu@ip-172-31-74-193.ec2.internal>
2022-04-01 02:47:20 +0000

    Stop Cam after post

--- src/App.js
+++ src/App.js
@@ -55,14 +55,15 @@ class App extends React.Component {
               .then(res => {
                 console.log(res);
                 console.log(res.data);
+                  this.stop().then((ignore) => {
+                  // QR Code scanning is stopped.
+                  console.log("ignore: " + ignore);
+                  }).catch((err) => {
+                  // Stop failed, handle it.
+                  console.log("error stoping cam" + err);
+                  });
               })
-            this.stop().then((ignore) => {
-              // QR Code scanning is stopped.
-              console.log("ignore: " + ignore);
-            }).catch((err) => {
-              // Stop failed, handle it.
-              console.log("error stoping cam" + err);
-            });
+
    
       
     this.setState((state, props) => {
